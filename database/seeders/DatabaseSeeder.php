3a43681358df99194dca7841ef895130b67b7d58
Muacidevjava <muaci@yahoo.com>
2025-04-03 15:34:45 -0300

    tabela-banco-configurando-model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             StatusSeeder::class,
             CategoriaSeeder::class,
-            UnidadeSeeder::class
+            UnidadeSeeder::class,
+            BancoSeeder::class
         ]);
     }
 }
