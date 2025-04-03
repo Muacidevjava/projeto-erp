923b547988c8e33ce316d4cfa002a0e1e3976df2
Muacidevjava <muaci@yahoo.com>
2025-04-03 08:55:58 -0300

    Tabela Categoria - Configurando Model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            StatusSeeder::class
+            StatusSeeder::class,
+            CategoriaSeeder::class,
         ]);
     }
 }
