eb1ba4495486aed2100231c76d7be822a311c0e0
Muacidevjava <muaci@yahoo.com>
2025-04-03 16:00:25 -0300

    banco-listagem

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Cadastro\BancoController;
 use App\Http\Controllers\Cadastro\CategoriaController;
 use App\Http\Controllers\Cadastro\StatusController;
 use App\Http\Controllers\Cadastro\UnidadeController;
@@ -13,3 +14,4 @@ Route::get('/', function () {
 Route::resource("/status", StatusController::class);
 Route::resource("/categoria", CategoriaController::class);
 Route::resource("/unidade", UnidadeController::class);
+Route::resource("/banco", BancoController::class);
