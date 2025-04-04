c574713a88da7105cdd8ebe77904906fba5c7f49
Muacidevjava <muaci@yahoo.com>
2025-04-04 14:07:08 -0300

    criando alasse modal i o include do Create

--- resources/views/cadastro/Produto/Create.blade.php
+++ resources/views/cadastro/Produto/Create.blade.php
@@ -131,26 +131,6 @@ use App\Service\ConstanteService;
     </div>
 
 </div>
-<div class="window medio" id="modalCategoria">
-    <span class="d-block titulo mb-0 h4"><i class="fas fa-plus-circle"></i> Adicionar nova categoria</span>
-
-    <div class="p-3">
-        <div class="rows">
-            <div class="col-12">
-                <label class="text-label d-block">Nome </label>
-                <input type="text" name="categoria" id="txtCategoria" class="form-campo"
-                    placeholder="Inserir categoria">
-            </div>
-        </div>
-    </div>
-    <div class="tfooter end mt-4">
-        <a href="javascript:;" onclick="fecharModal()" class="btn btn-outline-amarelo ">Cancelar</a>
-        <input type="button" onclick="salvarCategoria()" value="Inserir categoria"
-            class="btn btn-roxo text-uppercase">
-    </div>
-</div>
-
-
-
+@include('Cadastro.Categoria.modalCategoria')
 
 @endsection
