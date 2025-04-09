1816d9b670626beaf43c12458a926d1b44953392
Muacidevjava <muaci@yahoo.com>
2025-04-09 09:48:04 -0300

    colocando mascara no celular

--- resources/views/cadastro/Cliente/Create.blade.php
+++ resources/views/cadastro/Cliente/Create.blade.php
@@ -96,7 +96,7 @@
                                 </div>
                                 <div class="col-4 mb-3">
                                     <label class="text-label">Celular:</label>
-                                    <input type="text" name="celular" value="{{ old('celular') }}" class="form-campo">
+                                    <input type="text" name="celular" value="{{ old('celular') }}" class="form-campo mascara-celular">
                                 </div>
 
                                 <div class="col-8 mb-3">
