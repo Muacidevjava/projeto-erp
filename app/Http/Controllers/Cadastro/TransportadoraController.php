ed9f449271349a21a1cf4816e16479d24a83d06b
Muacidevjava <muaci@yahoo.com>
2025-04-10 15:32:15 -0300

    Criado o controller

---
+++ app/Http/Controllers/Cadastro/TransportadoraController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Cadastro;
+
+use App\Http\Controllers\Controller;
+use App\Models\Transportadora;
+use Illuminate\Http\Request;
+use stdClass;
+
+class TransportadoraController extends Controller
+{
+    public function index()
+    {
+       
+       
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function store(Request $request)
+    {
+        
+    }
+
+    public function edit(string $id)
+    {
+       
+    }
+
+    public function update(Request $request, string $id)
+    {
+       
+    }
+
+    public function destroy(string $id)
+    {
+       
+    }
+}
